4c625cbe3fc20f77333847dcdb1912cbfc1c754b
TTE <scion0214uni@gmail.com>
2023-01-28 21:07:37 +0900

    랜덤위치에 블럭생성

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    List<List<Block>> blocks;
+    Block[,] blocks;
     [SerializeField]
     private GameObject BlockPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        Resources.Load<GameObject>("Prefabs/block");
+        blocks = new Block[4, 4];
+        CreateBlock();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreateBlock() {
+        int LocationX;
+        int LocationY;
+
+        LocationX = Random.Range(0, 4);
+        LocationY = Random.Range(0, 4);
+
+        GameObject newBlock = Instantiate(BlockPrefab, new Vector3(LocationX - 1.5f, LocationY - 1.5f, 0f), Quaternion.identity);
+
+        blocks[LocationX, LocationY] = newBlock.GetComponent<Block>();
     }
 }
