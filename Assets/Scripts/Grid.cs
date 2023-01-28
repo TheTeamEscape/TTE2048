ae44aed864744b191953852f7abe5b4e6b0dca56
TTE <scion0214uni@gmail.com>
2023-01-28 23:58:24 +0900

    방향조절가능

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -56,6 +56,123 @@ public class Grid : MonoBehaviour
                 }
             }
         }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            for (int i = 3; i > -1; --i)
+            {
+                int headNullY = 4;
+                for (int j = 3; j > -1; --j)
+                {
+                    if (headNullY == 4)
+                    {
+                        if (blocks[i, j] is null)
+                        {
+
+                            headNullY = j;
+
+                        }
+                    }
+                    else if (blocks[i, j] is not null)
+                    {
+
+                        blocks[i, headNullY] = blocks[i, j];
+                        blocks[i, j] = null;
+
+
+                        float x = blocks[i, headNullY].transform.position.x;
+                        float y = blocks[i, headNullY].transform.position.y;
+                        float z = blocks[i, headNullY].transform.position.z;
+
+                        y = headNullY - 1.5f;
+                        blocks[i, headNullY].transform.position = new Vector3(x, y, z);
+                        headNullY = headNullY - 1;
+
+                    }
+
+
+
+                }
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            for (int j = 3; j > -1; --j)
+            {
+                int headNullX = 4;
+                for (int i = 3; i > -1; --i)
+                {
+                    if (headNullX == 4)
+                    {
+                        if (blocks[i, j] is null)
+                        {
+
+                            headNullX = i;
+
+                        }
+                    }
+                    else if (blocks[i, j] is not null)
+                    {
+
+                        blocks[headNullX, j] = blocks[i, j];
+                        blocks[i, j] = null;
+
+
+                        float x = blocks[headNullX, j].transform.position.x;
+                        float y = blocks[headNullX, j].transform.position.y;
+                        float z = blocks[headNullX, j].transform.position.z;
+
+                        x = headNullX - 1.5f;
+                        blocks[headNullX, j].transform.position = new Vector3(x, y, z);
+                        headNullX = headNullX - 1;
+
+                    }
+
+
+
+                }
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                int headNullX = -1;
+                for (int i = 0; i < 4; ++i)
+                {
+                    if (headNullX == -1)
+                    {
+                        if (blocks[i, j] is null)
+                        {
+
+                            headNullX = i;
+
+                        }
+                    }
+                    else if (blocks[i, j] is not null)
+                    {
+
+                        blocks[headNullX, j] = blocks[i, j];
+                        blocks[i, j] = null;
+
+
+                        float x = blocks[headNullX, j].transform.position.x;
+                        float y = blocks[headNullX, j].transform.position.y;
+                        float z = blocks[headNullX, j].transform.position.z;
+
+                        x = headNullX - 1.5f;
+                        blocks[headNullX, j].transform.position = new Vector3(x, y, z);
+                        headNullX = headNullX + 1;
+
+                    }
+
+
+
+                }
+            }
+        }
     }
 
     public void CreateBlock() {
